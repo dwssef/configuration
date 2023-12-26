29851221f57e8a99f8c3adcf01080713b6b972c8
dwssef <dwssef@protonmail.com>
2023-12-26 14:53:08 +0800

    Update go_setup.sh - Wget use ipv4 - Replace with the official installation - Modify env write way

--- go_setup.sh
+++ go_setup.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-VERSION="1.17.1"
+VERSION="1.21.5"
 
-source .bashrc
-wget https://studygolang.com/dl/golang/go$VERSION.linux-amd64.tar.gz
-tar xvfz go$VERSION.linux-amd64.tar.gz
-rm -rf /usr/local/go
-mv go /usr/local/
+wget -4 https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
 
-# go get golang.org/x/tools/cmd/goimports
-# go get github.com/nsf/gocode
-
-rm go$VERSION.linux-amd64.tar.gz -rf
-
-
-tee -a $HOME/.bashrc <<'EOF'
+tee -a $HOME/.zshrc <<'EOF'
 export GOPROXY=https://goproxy.cn,direct
 export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=$PATH:/usr/local/go/bin
 EOF
